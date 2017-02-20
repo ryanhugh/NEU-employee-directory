f3a3eea13ac0d1d28ec394e0d62d22d7263fdbd2
Ryan Hughes <rysquash@gmail.com>
2017-02-19 20:55:20 -0800

    .

---
+++ travis_scrape.sh
@@ -0,0 +1,21 @@
+if [ "$TRAVIS_EVENT_TYPE" != "cron2" ]; 
+  exit 0
+fi
+
+
+eval "$(ssh-agent -s)"
+echo $GIT_PRIVATE_KEY | base64 --decode > ~/deploy_key
+chmod 600 ~/deploy_key
+ssh-add ~/deploy_key
+git checkout -b gh-pages
+echo $TRAVIS_EVENT_TYPE
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; 
+  then node main.js; 
+fi
+git merge master
+git config --global user.email "ryanhughes624+gitbot@gmail.com"
+git config --global user.name "data-updater-bot"
+git commit -a -m "test commit!"
+git remote add deploy git@github.com:ryanhugh/NEU-employee-directory.git
+git config --global push.default simple
+git push git@github.com:ryanhugh/NEU-employee-directory.git gh-pages
