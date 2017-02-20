72f47222e3dbb8b935608fb195ae90e3593923be
Ryan Hughes <rysquash@gmail.com>
2017-02-19 21:17:03 -0800

    .

--- travis_scrape.sh
+++ travis_scrape.sh
@@ -1,5 +1,9 @@
 # Pull requests and commits to other branches shouldn't try to deploy
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo hi
+    echo $TRAVIS_PULL_REQUEST
+    echo $TRAVIS_BRANCH
+    echo $SOURCE_BRANCH
     exit 0
 fi
 
