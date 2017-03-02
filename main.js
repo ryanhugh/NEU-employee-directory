1462351ed972c8ea9701a6858165cf493a7b42c6
Ryan Hughes <rysquash@gmail.com>
2017-03-02 00:50:32 -0800

    .

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ var cookie = require('cookie')
 
 var alphabet = 'qwertyuiopasdfghjklzxcvbnm';
 
-// if (process.env.TRAVIS_EVENT_TYPE != 'cron' && process.env.TRAVIS) {
-// 	console.log('not running on travis event', process.env.TRAVIS_EVENT_TYPE)
-// 	process.exit(0);
-// }
+if (process.env.TRAVIS_EVENT_TYPE != 'cron' && process.env.TRAVIS) {
+	console.log('not running on travis event', process.env.TRAVIS_EVENT_TYPE)
+	process.exit(0);
+}
 
 
 function handleRequestResponce(body, callback) {
