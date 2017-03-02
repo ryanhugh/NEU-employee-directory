235bbf94d6154da38afc153deaf0234de67545f3
Ryan Hughes <rysquash@gmail.com>
2017-03-02 00:19:13 -0800

    travis changes.

--- travis_scrape.sh
+++ travis_scrape.sh
@@ -14,11 +14,12 @@ echo $GIT_PRIVATE_KEY | base64 --decode > ~/deploy_key
 chmod 600 ~/deploy_key
 ssh-add ~/deploy_key
 git checkout -b gh-pages
-node main.js
+git pull origin gh-pages
+node main.js	
 git merge master
 git config --global user.email "ryanhughes624+gitbot@gmail.com"
 git config --global user.name "data-updater-bot"
 git commit -a -m "Updated data"
 git remote add deploy git@github.com:ryanhugh/NEU-employee-directory.git
 git config --global push.default simple
-git push git@github.com:ryanhugh/NEU-employee-directory.git gh-pages --force
+git push git@github.com:ryanhugh/NEU-employee-directory.git gh-pages
